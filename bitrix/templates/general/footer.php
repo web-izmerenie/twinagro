88d95b516d78b1564324aff1ab63ded872b4ac1a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-12-28 21:08:36 +0300

    refactoring of header.php and footer.php

--- bitrix/templates/general/footer.php
+++ bitrix/templates/general/footer.php
@@ -1,40 +1,44 @@
 
-        </div>
-    </section>
+		</div>
+	</section>
 
-    <footer>
-        <hr />
-        <div id="author">
-            <span class="ico-author sprite-ico"></span>
-            <?$APPLICATION->IncludeFile($APPLICATION->GetTemplatePath("include_areas/".LANGUAGE_ID."/copyright.php"), Array(), Array("MODE" => "php"));?>
-        </div>
-    </footer>
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter27005949 = new Ya.Metrika({id:27005949,
-                        webvisor:true,
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true});
-            } catch(e) { }
-        });
+	<footer>
+		<hr />
+		<div id="author">
+			<span class="ico-author sprite-ico"></span>
+			<?$APPLICATION->IncludeFile(
+				$APPLICATION->GetTemplatePath(
+					"include_areas/".LANGUAGE_ID."/copyright.php"),
+					Array(),
+					Array("MODE" => "php"));?>
+		</div>
+	</footer>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+	(function (d, w, c) {
+		(w[c] = w[c] || []).push(function() {
+			try {
+				w.yaCounter27005949 = new Ya.Metrika({id:27005949,
+						webvisor:true,
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true});
+			} catch(e) { }
+		});
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+		var n = d.getElementsByTagName("script")[0],
+			s = d.createElement("script"),
+			f = function () { n.parentNode.insertBefore(s, n); };
+		s.type = "text/javascript";
+		s.async = true;
+		s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
 
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-    </script>
-    <noscript><div><img src="//mc.yandex.ru/watch/27005949" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
+		if (w.opera == "[object Opera]") {
+			d.addEventListener("DOMContentLoaded", f, false);
+		} else { f(); }
+	})(document, window, "yandex_metrika_callbacks");
+	</script>
+	<noscript><div><img src="//mc.yandex.ru/watch/27005949" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 </body>
 </html>
--- bitrix/templates/general/header.php
+++ bitrix/templates/general/header.php
@@ -1,64 +1,64 @@
 <?
-    $rsSite = CSite::GetByID(SITE_ID);
-    $arSite = $rsSite->Fetch();
+	$rsSite = CSite::GetByID(SITE_ID);
+	$arSite = $rsSite->Fetch();
 
-    if ($APPLICATION->GetCurPage(false) == '/') :
-        define('MAIN_PAGE', 'Y');
-    endif;
+	if ($APPLICATION->GetCurPage(false) == '/') :
+		define('MAIN_PAGE', 'Y');
+	endif;
 
-    $revision = 17;
-    if ($USER->IsAdmin()) $revision = $revision . 'dev' . 11;
+	$revision = 17;
+	if ($USER->IsAdmin()) $revision = $revision . 'dev' . 11;
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?=LANGUAGE_ID
 ?>" lang="<?=LANGUAGE_ID?>" class="no-js<?
-    defined('MAIN_PAGE') && print(' page-main');
-    defined('ERROR_404') && print(' error-page');
+	defined('MAIN_PAGE') && print(' page-main');
+	defined('ERROR_404') && print(' error-page');
 ?>">
 <head>
-    <meta charset="<?=LANG_CHARSET?>" />
-    <title><?$APPLICATION->IncludeFile(
-        $APPLICATION->GetTemplatePath("include_areas/page_title.php"),
-        array(),
-        array("MODE" => "PHP", "SHOW_BORDER" => false)
-    );?><?=$arSite["SITE_NAME"]?></title>
-    <?$APPLICATION->ShowMeta("keywords")?>
-    <?$APPLICATION->ShowMeta("description")?>
+	<meta charset="<?=LANG_CHARSET?>" />
+	<title><?$APPLICATION->IncludeFile(
+		$APPLICATION->GetTemplatePath("include_areas/page_title.php"),
+		array(),
+		array("MODE" => "PHP", "SHOW_BORDER" => false)
+	);?><?=$arSite["SITE_NAME"]?></title>
+	<?$APPLICATION->ShowMeta("keywords")?>
+	<?$APPLICATION->ShowMeta("description")?>
 
-    <link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/styles/build/build.css?v=<?=$revision?>" />
+	<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/styles/build/build.css?v=<?=$revision?>" />
 
-    <script src="<?=SITE_TEMPLATE_PATH?>/scripts/build/build.js?v=<?=$revision?>"></script>
-    <script>
-        //<![CDATA[
-            require(['basics/get_val'], function (getVal) {
+	<script src="<?=SITE_TEMPLATE_PATH?>/scripts/build/build.js?v=<?=$revision?>"></script>
+	<script>
+		//<![CDATA[
+			require(['basics/get_val'], function (getVal) {
 
-                getVal.set('tplPath', '<?=SITE_TEMPLATE_PATH?>');
-                getVal.set('lang', '<?=LANGUAGE_ID?>');
-                getVal.set('revision', '<?=$revision?>');
+				getVal.set('tplPath', '<?=SITE_TEMPLATE_PATH?>');
+				getVal.set('lang', '<?=LANGUAGE_ID?>');
+				getVal.set('revision', '<?=$revision?>');
 
-                require(['main']);
+				require(['main']);
 
-            });
-        //]]>
-    </script>
+			});
+		//]]>
+	</script>
 
-    <?$APPLICATION->ShowCSS()?>
-    <?$APPLICATION->ShowHeadStrings()?>
-    <?$APPLICATION->ShowHeadScripts()?>
+	<?$APPLICATION->ShowCSS()?>
+	<?$APPLICATION->ShowHeadStrings()?>
+	<?$APPLICATION->ShowHeadScripts()?>
 </head>
 
 <body>
-    <div id="bitrix_panel"><?$APPLICATION->ShowPanel()?></div>
+	<div id="bitrix_panel"><?$APPLICATION->ShowPanel()?></div>
 
-    <header>
-        <div class="logo" title="<?=$arSite["SITE_NAME"]?>">
-            <?if ( ! defined("MAIN_PAGE")) :?><a href="/"><?endif?>
-                <img alt="<?=$arSite["SITE_NAME"]?>" src="<?=SITE_TEMPLATE_PATH?>/images/twinagro_logo_white_on_dark.png" class="white" />
-                <img alt="<?=$arSite["SITE_NAME"]?>" src="<?=SITE_TEMPLATE_PATH?>/images/twinagro_logo_dark_on_white.png" class="dark" />
-            <?if ( ! defined("MAIN_PAGE")) :?></a><?endif?>
-        </div>
+	<header>
+		<div class="logo" title="<?=$arSite["SITE_NAME"]?>">
+			<?if ( ! defined("MAIN_PAGE")) :?><a href="/"><?endif?>
+				<img alt="<?=$arSite["SITE_NAME"]?>" src="<?=SITE_TEMPLATE_PATH?>/images/twinagro_logo_white_on_dark.png" class="white" />
+				<img alt="<?=$arSite["SITE_NAME"]?>" src="<?=SITE_TEMPLATE_PATH?>/images/twinagro_logo_dark_on_white.png" class="dark" />
+			<?if ( ! defined("MAIN_PAGE")) :?></a><?endif?>
+		</div>
 
-        <?$APPLICATION->IncludeComponent("bitrix:menu", "main_menu", array(
+		<?$APPLICATION->IncludeComponent("bitrix:menu", "main_menu", array(
 	"ROOT_MENU_TYPE" => "top",
 	"MENU_CACHE_TYPE" => "A",
 	"MENU_CACHE_TIME" => "3600",
@@ -74,9 +74,9 @@
 	false
 );?>
 
-    </header>
+	</header>
 
-    <section class="content">
+	<section class="content">
 		<div style="display:none;">
 			<ul>
 				<li><h3>в Липецке</h3></li>
@@ -87,8 +87,8 @@
 				<li><h3>в Ростове-на-Дону</h3></li>
 			</ul>
 		</div>
-        <?if (!defined('MAIN_PAGE') && !defined('ERROR_404')) :?>
-            <h1 class="page_title"><span></span><span><?=$APPLICATION->ShowTitle()?></span><span></span></h1>
-        <?endif?>
-        <div class="page_content" <?if (defined('MAIN_PAGE')) :?> id="main"<?endif?>>
+		<?if (!defined('MAIN_PAGE') && !defined('ERROR_404')) :?>
+			<h1 class="page_title"><span></span><span><?=$APPLICATION->ShowTitle()?></span><span></span></h1>
+		<?endif?>
+		<div class="page_content" <?if (defined('MAIN_PAGE')) :?> id="main"<?endif?>>
 
